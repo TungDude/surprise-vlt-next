6bae23606fb1b3013971a9cd98f212f2456843a7
Tung <krittapop.kms@gmail.com>
2025-02-11 00:24:22 +0700

    feat: itim font

--- vlt-next-app/src/app/layout.tsx
+++ vlt-next-app/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Itim } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const itim = Itim({
+  variable: "--font-itim",
+  subsets: ["latin", "thai"],
+  weight: "400", 
 });
 
 export const metadata: Metadata = {
@@ -25,12 +31,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${itim.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 py-20 gap-16 sm:p-20 font-[family-name:var(--font-itim)]">
+          <header className="row-start-1 flex gap-6 flex-wrap items-center justify-center">
+            Header
+          </header>
+          <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
             {children}
           </main>
+          <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+            Footer
+          </footer>
         </div>
       </body>
     </html>
