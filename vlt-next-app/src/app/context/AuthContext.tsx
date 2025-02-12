2760b17f53a14781f0d74a48e676b942631d409b
Tung <krittapop.kms@gmail.com>
2025-02-12 21:14:16 +0700

    fix: authroutes array

--- vlt-next-app/src/app/context/AuthContext.tsx
+++ vlt-next-app/src/app/context/AuthContext.tsx
@@ -4,24 +4,33 @@ import RequestController from "../lib/RequestController";
 
 interface AuthContextType {
     verifyPasscode: (code: string) => void;
-    authenticatedRoute: string | null;
+    authenticatedRoutes: string[] | null;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    const [authenticatedRoute, setAuthenticatedRoute] = useState<string | null>(null);
+    const [authenticatedRoutes, setAuthenticatedRoutes] = useState<string[]>([]);
 
     const verifyPasscode = async (code: string) => {
         const response = await RequestController.validatePasscode(code);
-        
+
         if (response.valid) {
-            setAuthenticatedRoute(response.route);
+            setAuthenticatedRoutes((prevRoutes) => {
+                // Check if route is already in the array
+                if (!prevRoutes.includes(response.route)) {
+                    return [...prevRoutes, response.route]; // Add new route
+                }
+                return prevRoutes; // Return same array if duplicate
+            });
         }
+
+        console.log(authenticatedRoutes);
     };
 
+
     return (
-        <AuthContext.Provider value={{ verifyPasscode, authenticatedRoute }}>
+        <AuthContext.Provider value={{ verifyPasscode, authenticatedRoutes }}>
             {children}
         </AuthContext.Provider>
     );
