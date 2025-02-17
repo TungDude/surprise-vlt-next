3e3b2ef3e3dacf62df31353ec2a12dc75966ff79
Tung <krittapop.kms@gmail.com>
2025-02-17 20:47:57 +0700

    feat: home button footer

--- vlt-next-app/src/app/components/Footer/Footer.tsx
+++ vlt-next-app/src/app/components/Footer/Footer.tsx
@@ -1,13 +1,17 @@
 "use client"
+import HomeButton from "../Button/HomeButton/HomeButton";
 
 function Footer () {
     return (
         <>
             <div>
-                ❤️❤️❤️❤️❤️
+                ❤️❤️❤️
             </div>
             <div>
-                ❤️❤️❤️❤️❤️
+                <HomeButton />
+            </div>
+            <div>
+                ❤️❤️❤️
             </div>
         </>
     )
