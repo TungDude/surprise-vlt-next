9efcb82d56caaa34141b7ed380b0c189d0ba65c9
Tung <krittapop.kms@gmail.com>
2025-02-17 21:34:20 +0700

    feat: routing footer hidden

--- vlt-next-app/src/app/components/Footer/Footer.tsx
+++ vlt-next-app/src/app/components/Footer/Footer.tsx
@@ -1,16 +1,59 @@
 "use client"
+// import { useRouter } from "next/navigation";
 import HomeButton from "../Button/HomeButton/HomeButton";
+import { useAuthContext } from "@/app/context/AuthContext";
+// import { Heart, Gift } from 'lucide-react';
+import { cn } from "@/app/lib/utils";
 
 function Footer () {
+    // const router = useRouter();
+    const { authenticatedRoutes } = useAuthContext();
+    const vAllowed = authenticatedRoutes?.some(route => route.startsWith(`valentine`));
+    const aAllowed = authenticatedRoutes?.some(route => route.startsWith(`anniversary`));
+
+    // const handleClickHeart = () => {
+    //     if (!vAllowed) {
+    //         return;
+    //     }
+
+    //     router.push("/valentine");
+    // }
+
+    // const handleClickGift = () => {
+    //     if (!aAllowed) {
+    //         return;
+    //     }
+
+    //     router.push("/anniversary");
+    // }
+
     return (
         <>
-            <div>
+            <div
+                className={cn(
+                    vAllowed ? "text-brightred hover:text-black" : "text-gray-400"
+                )}
+                // onClick={handleClickHeart}
+            >
+                {/* <Heart 
+                    strokeWidth={2}
+                    className="w-12 h-12 cursor-pointer"
+                /> */}
                 ❤️❤️❤️
             </div>
             <div>
                 <HomeButton />
             </div>
-            <div>
+            <div
+                className={cn(
+                    aAllowed ? "text-pink hover:text-black" : "text-gray-400"
+                )}
+                // onClick={handleClickGift}
+            >
+                {/* <Gift 
+                    strokeWidth={2}
+                    className="w-12 h-12 cursor-pointer"
+                /> */}
                 ❤️❤️❤️
             </div>
         </>
