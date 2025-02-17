6bdd266035e8d22d458829dcb04f9d06131e9458
Tung <krittapop.kms@gmail.com>
2025-02-17 20:47:35 +0700

    fix: button minor

--- vlt-next-app/src/app/components/Button/Button.tsx
+++ vlt-next-app/src/app/components/Button/Button.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import { ReactNode } from "react";
 import { cn } from "../../lib/utils";
 
 interface ButtonProps {
-    label: string;
+    label: ReactNode;
     onClick?: React.MouseEventHandler<HTMLButtonElement>; // Correct type for onClick
-    variant?: "primary" | "secondary" | "blackwhite" | "warning" | "blueboy"; // Restrict variants to valid strings
+    variant?: "primary" | "secondary" | "blackwhite" | "warning" | "blueboy" | "transparent"; // Restrict variants to valid strings
     className?: string;
     disabled?: boolean,
 }
 
 const Button = ({ label, onClick, variant = "primary", className, disabled = false }: ButtonProps) => {
-    const baseClasses = "text-white py-3 px-8 rounded-lg";
+    const baseClasses = "text-white py-3 px-8 rounded-lg shadow-md";
     const variantClasses = {
         primary: "bg-green hover:bg-darkgreen",
         secondary: "bg-lightgray hover:bg-black",
         warning: "bg-brightred hover:bg-darkred", 
-        blackwhite: "bg-black",
+        blackwhite: "bg-white text-black border hover:text-white hover:bg-black",
         blueboy: "bg-lightblue hover:bg-blue",
+        transparent: "border bg-transparent hover:bg-gray-100 active:bg-gray-200"
     };
 
     return (
